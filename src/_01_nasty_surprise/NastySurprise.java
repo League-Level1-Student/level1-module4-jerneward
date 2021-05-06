837a815b86cc5590436e2d60bbe19eebd8d8ddf2
student <student@ip-172-31-61-229.us-west-2.compute.internal>
2021-05-05 17:00:27 -0700

    1

--- src/_01_nasty_surprise/NastySurprise.java
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -1,5 +1,66 @@
 package _01_nasty_surprise;
 
-public class NastySurprise {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-}
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class NastySurprise implements ActionListener {
+	
+	JFrame frame = new JFrame();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	
+	
+	public void nastysurprise() {
+		
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		button1.setText("Trick");
+		button2.setText("Treat");
+		frame.pack();
+		frame.add(button1);
+		frame.add(button2);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		
+	}
+	
+	
+	
+		
+		
+		private void showPictureFromTheInternet(String imageUrl) {
+		    try {
+		        URL url = new URL(monster.png);
+		        Icon icon = new ImageIcon(url);
+		        JLabel imageLabel = new JLabel(icon);
+		        JFrame frame = new JFrame();
+		        frame.add(imageLabel);
+		        frame.setVisible(true);
+		        frame.pack();
+		    } catch (MalformedURLException e) {
+		        e.printStackTrace();
+		    }
+		}
+		
+		public void actionPerformed () {
+			
+			if
+			
+			
+		}
+
+
+
+
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
--- src/_01_nasty_surprise/NastySurpriseRunner.java
+++ src/_01_nasty_surprise/NastySurpriseRunner.java
@@ -2,4 +2,8 @@ package _01_nasty_surprise;
 
 public class NastySurpriseRunner {
 
+	public static void main(String[] args) {
+		
+	}
+	
 }
